c372146fb573de4ce1ea591b2decb34d6a879a9f
Thomas Ochman <thomas@nocebo.se>
2013-12-05 23:27:42 +0100

    Added image resources and worked on footer layout disposition

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -3,8 +3,16 @@ class VisitorsController < ApplicationController
 
   def new
     @features = Project.where(:public => true)
-
+    @repos = get_repos
     #@projects = Project.all
+  end
+
+  def get_repos
+    url = 'https://api.github.com/orgs/AgileVentures/repos'
+
+    response = Net::HTTP.get_response(URI.parse(url))
+    data = response.body
+    @repos = JSON.parse(data)
   end
 
   def index
