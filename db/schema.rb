ed17b6f7213bd30cb9b6f40a7ac673c65c063638
Thomas Ochman <thomas@nocebo.se>
2013-12-22 13:43:32 +0100

    Added fields to Devise and restyled Devise forms

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131201214926) do
+ActiveRecord::Schema.define(version: 20131222121724) do
 
   create_table "projects", force: true do |t|
     t.string   "title"
@@ -35,6 +35,10 @@ ActiveRecord::Schema.define(version: 20131201214926) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "avatar"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "organization"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
