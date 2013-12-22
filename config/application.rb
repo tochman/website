fdbb38dd8ec85b49fd7ef703180c7b10beac27b7
Thomas Ochman <thomas@nocebo.se>
2013-12-22 11:58:26 +0100

    added features to test Devise

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,15 @@ Bundler.require(:default, Rails.env)
 module Website
   class Application < Rails::Application
     config.exceptions_app = self.routes
+    config.generators do |g|
+      g.test_framework :rspec, fixture: true
+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+      g.view_specs false
+      g.helper_specs false
+      g.stylesheets = false
+      g.javascripts = false
+      g.helper = false
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
