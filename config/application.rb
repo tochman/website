2807601b4effea2e9cb47689dc4f5bc900877c7e
Thomas Ochman <thomas@nocebo.se>
2013-12-23 18:46:43 +0100

    Carrierwave-dropbox is working and storing files in my personal account

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,8 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
+require 'dotenv'
+Dotenv.load
 
 
 # Require the gems listed in Gemfile, including any gems
@@ -32,3 +34,79 @@ module Website
     # config.i18n.default_locale = :de
   end
 end
+
+require 'dropbox_sdk'
+
+module CarrierWave
+  module Storage
+    class Dropbox < Abstract
+
+      # Stubs we must implement to create and save
+      # files (here on Dropbox)
+
+      # Store a single file
+      def store!(file)
+        location = uploader.store_path
+        location = "/Public/#{location}" if config[:access_type] == "dropbox"
+
+        dropbox_client.put_file(location, file.to_file)
+      end
+
+      # Retrieve a single file
+      def retrieve!(file)
+        CarrierWave::Storage::Dropbox::File.new(uploader, config, uploader.store_path(file), dropbox_client)
+      end
+
+      private
+
+      def dropbox_client
+        @dropbox_client ||= begin
+          session = DropboxSession.new(config[:app_key], config[:app_secret])
+          session.set_access_token(config[:access_token], config[:access_token_secret])
+          DropboxClient.new(session, config[:access_type])
+        end
+      end
+
+      def config
+        @config ||= {}
+
+        @config[:app_key] = "chjxxbhpojosbt3"
+        @config[:app_secret] = "wlton1e4b7zwc7o"
+        @config[:access_token] = "t9iznd2jdniaob89"
+        @config[:access_token_secret] = "fj8m1eg7u3v89zg"
+        @config[:access_type] = "app_folder"
+        @config[:user_id] = "52190197"
+
+        @config
+      end
+
+      class File
+        include CarrierWave::Utilities::Uri
+        attr_reader :path
+
+        def initialize(uploader, config, path, client)
+          @uploader, @config, @path, @client = uploader, config, path, client
+        end
+
+        def url
+          user_id, path = @config[:user_id], @path
+          if @config[:access_type] == "dropbox"
+            "https://dl.dropboxusercontent.com/u#{user_id}/#{path}"
+          else
+            @client.media(path)["url"]
+          end
+        end
+
+        def delete
+          path = @path
+          path = "/Public/#{path}" if @config[:access_type] == "dropbox"
+          begin
+            @client.file_delete(path)
+          rescue DropboxError
+          end
+        end
+      end
+    end
+  end
+end
+
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,20 +1,8 @@
-CarrierWave.configure do |config|
-  config.dropbox_app_key = ENV["APP_KEY"]
-  config.dropbox_app_secret = ENV["APP_SECRET"]
-  config.dropbox_access_token = ENV["ACCESS_TOKEN"]
-  config.dropbox_access_token_secret = ENV["ACCESS_TOKEN_SECRET"]
-  config.dropbox_user_id = ENV["USER_ID"]
-  config.dropbox_access_type = "app_folder"
-  debugger
-end
-
-
-
 #CarrierWave.configure do |config|
-#  config.dropbox_app_key = ENV["chjxxbhpojosbt3"]
-#  config.dropbox_app_secret = ENV["wlton1e4b7zwc7o"]
-#  config.dropbox_access_token = ENV["t9iznd2jdniaob89"]
-#  config.dropbox_access_token_secret = ENV["fj8m1eg7u3v89zg"]
-#  config.dropbox_user_id = ENV["52190197"]
-#  config.dropbox_access_type = ENV["app_folder"]
+#  config.dropbox_app_key = 'chjxxbhpojosbt3'
+#  config.dropbox_app_secret = 'wlton1e4b7zwc7o'
+#  config.dropbox_access_token = 't9iznd2jdniaob89'
+#  config.dropbox_access_token_secret = 'fj8m1eg7u3v89zg'
+#  config.dropbox_user_id = '52190197'
+#  config.dropbox_access_type = 'app_folder'
 #end
