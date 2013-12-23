7366dd5a111a96f97411ec719f98cdbe3bba30a2
Thomas Ochman <thomas@nocebo.se>
2013-12-23 10:10:47 +0100

    config/production.rb: config.assets.initialize_on_precompile = false

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,6 +29,7 @@ Website::Application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
+  config.assets.initialize_on_precompile = false
   # Generate digests for assets URLs.
   config.assets.digest = true
 
