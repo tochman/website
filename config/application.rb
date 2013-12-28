7436f47fca12550d3252055cab07cc045450792b
Thomas Ochman <thomas@nocebo.se>
2013-12-28 11:40:58 +0100

    Another rollback and hopefully a fix for carrierwave-dropbox problems 2

--- config/application.rb
+++ config/application.rb
@@ -39,26 +39,7 @@ module CarrierWave
   module Storage
     class Dropbox < Abstract
 
-      # Stubs we must implement to create and save
-      # files (here on Dropbox)
-
-      # Store a single file
-      def store!(file)
-        location = uploader.store_path
-        location = "/Public/#{location}" if config[:access_type] == "dropbox"
-        dropbox_client.put_file(location, file.to_file)
-      end
-
-
       private
-
-      def dropbox_client
-        @dropbox_client ||= begin
-          session = DropboxSession.new(config[:app_key], config[:app_secret])
-          session.set_access_token(config[:access_token], config[:access_token_secret])
-          DropboxClient.new(session, config[:access_type])
-        end
-      end
 
       def config
         @config ||= {}
@@ -74,32 +55,6 @@ module CarrierWave
         @config
       end
 
-      class File
-        include CarrierWave::Utilities::Uri
-        attr_reader :path
-
-        def initialize(uploader, config, path, client)
-          @uploader, @config, @path, @client = uploader, config, path, client
-        end
-
-        def url
-          user_id, path = @config[:user_id], @path
-          if @config[:access_type] == "dropbox"
-            "https://dl.dropboxusercontent.com/u#{user_id}/#{path}"
-          else
-            @client.media(path)["url"]
-          end
-        end
-
-        def delete
-          path = @path
-          path = "/Public/#{path}" if @config[:access_type] == "dropbox"
-          begin
-            @client.file_delete(path)
-          rescue DropboxError
-          end
-        end
-      end
     end
   end
 end
