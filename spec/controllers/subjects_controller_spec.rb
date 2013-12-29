0254d3b03a767d1c1830c65a32d053e0f3b057c1
DV Suresh <e@dvsuresh.me>
2013-12-29 16:46:29 -0600

    add destroy, edit and update specs

--- spec/controllers/subjects_controller_spec.rb
+++ spec/controllers/subjects_controller_spec.rb
@@ -68,4 +68,84 @@ describe SubjectsController do
       end
     end
   end
+
+  describe 'DELETE #destroy' do
+    before :each do
+      @subject = create(:subject)
+      @controller.stub(:authenticate_user!).and_return(true)
+    end
+
+    it "deletes the subject" do
+      expect {
+        delete :destroy, id: @subject
+      }.to change(Subject, :count).by(-1)
+    end
+
+    it "redirects to subjects#index" do
+      delete :destroy, id: @subject
+      expect(response).to redirect_to subjects_path
+    end
+  end
+
+  describe 'GET #edit' do
+    before :each do
+      @subject = create(:subject)
+      @controller.stub(:authenticate_user!).and_return(true)
+    end
+
+    it "assigns the requested subject to @subject" do
+      get :edit, id: @subject
+      expect(assigns(:subject)).to eq @subject
+    end
+
+    it "renders the :edit template" do
+      get :edit, id: @subject
+      expect(response).to render_template :edit
+    end
+  end
+
+  describe 'PUT #update' do
+    before :each do 
+      @subject = create(:subject, name: 'Rails', description: 'Web Framework')
+      @controller.stub(:authenticate_user!).and_return(true)
+    end
+
+    context 'with valid attributes' do
+      it "located the requested @subject" do
+        patch :update, id: @subject, subject: attributes_for(:subject)
+        expect(assigns(:subject)).to eq @subject
+      end
+
+      it "changes @subject's attributes" do
+        patch :update, id: @subject,
+          subject: attributes_for(:subject,
+            name: 'Updated', description: 'Web Framework')
+        @subject.reload
+        expect(@subject.name).to eq 'Updated'
+        expect(@subject.description).to eq 'Web Framework'
+      end
+
+      it 'redirects to the updated subject' do
+        patch :update, id: @subject, subject: attributes_for(:subject)
+        expect(response).to redirect_to @subject
+      end
+    end
+
+    context 'with invalid attributes' do
+      it "does not change the subject's attributes" do 
+        patch :update, id: @subject,
+          subject: attributes_for(:subject,
+            name: nil, description: 'New')
+        @subject.reload
+        expect(@subject.name).to eq 'Rails'
+        expect(@subject.name).to_not eq 'New'
+      end
+
+      it 're-renders the #edit template' do
+        patch :update, id: @subject,
+          subject: attributes_for(:invalid_subject)
+        expect(response).to render_template :edit
+      end
+    end
+  end
 end
