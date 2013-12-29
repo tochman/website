d9fec13389d0953fb322800a6ec4b6e478134027
DV Suresh <e@dvsuresh.me>
2013-12-28 22:30:48 -0600

    refactor visitors controller spec

--- spec/controllers/visitors_controller_spec.rb
+++ spec/controllers/visitors_controller_spec.rb
@@ -4,9 +4,9 @@ require 'webmock/rspec'
 describe VisitorsController do
   describe 'GET #new' do
     before :each do
-      @project1 = FactoryGirl.create(:project, :public => true)
-      @project2 = FactoryGirl.create(:project, :public => true)
-      @project3 = FactoryGirl.create(:project, :public => false)
+      @project1 = create(:project, :public => true)
+      @project2 = create(:project, :public => true)
+      @project3 = create(:project, :public => false)
  
       stub_request(:get, "http://gdata.youtube.com/feeds/api/users/aeurdstfaksf/uploads?key=AI39si56zXaYYeIhrMack13lKTHuj95sOqfSHWkB2ASgI-XMH0PhI2hyl36vhS5zD2s85MkFhUBSObuNzeRBWfJn4ovqlqg5WA&max-results=25&start-index=1&v=2").
         to_return(:body => "", :status => 200, :headers => {})
