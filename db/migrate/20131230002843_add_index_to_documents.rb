77623c9fe477cf44bc978e7363f0ba04b4cd7a81
Thomas Ochman <thomas@nocebo.se>
2013-12-30 12:07:07 +0100

    How about this migration?

--- db/migrate/20131230002843_add_index_to_documents.rb
+++ db/migrate/20131230002843_add_index_to_documents.rb
@@ -1,5 +1,5 @@
 class AddIndexToDocuments < ActiveRecord::Migration
-
+  # TODO: This is clearly a mistake?!?
   def up
     add_index :documents, ["project_id"], name: "index_documents_on_project_id", using: :btree
   end
